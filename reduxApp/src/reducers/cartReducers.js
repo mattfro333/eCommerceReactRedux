60e9091e7f5fe97dd3138db905b39ae6915eb74c
mattfro333 <mattfro333@gmail.com>
2017-08-16 21:38:59 -0600

    A

--- reduxApp/src/reducers/cartReducers.js
+++ reduxApp/src/reducers/cartReducers.js
@@ -3,7 +3,7 @@
 export function cartReducers(state={cart:[]}, action){
   switch(action.type){
     case "ADD_TO_CART":
-     return {...state.cart, cart:action.payload}
+     return {cart:[...state.cart, ...action.payload]}
      break;
      case "DELETE_CART_ITEM":
       return {...state.cart, cart:action.payload}
