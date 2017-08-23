dd25ac0bb5410e7e2648367310cc73427f3acd04
mattfro333 <mattfro333@gmail.com>
2017-08-22 23:53:46 -0600

    added nav, footer, and router

--- reduxApp/src/app.js
+++ reduxApp/src/app.js
@@ -2,18 +2,37 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {applyMiddleware, createStore} from 'redux';
 import logger from 'redux-logger';
 import reducers from './reducers/index';
 import {addToCart} from './actions/cartActions';
 import {postBooks, deleteBooks, updateBooks} from './actions/booksActions';
+import Menu from './components/menu.js';
+import Footer from './components/footer.js';
+
 
 const middleware = applyMiddleware(logger);
 const store = createStore(reducers, middleware);
 
 import BooksList from './components/pages/bookslist';
+import Cart from './components/pages/cart';
+import BooksForm from './components/pages/booksForm';
+import Main from './main';
+const Routes = (
+ <Provider store={store}>
+ <Router history={browserHistory}>
+ <Route path="/" component={Main}>
+ <IndexRoute
+component={BooksList}/>
+ <Route path="/admin"
+component={BooksForm}/>
+ <Route path="/cart"
+component={Cart}/>
+ </Route>
+ </Router>
+ </Provider>
+)
 render(
-<Provider store={store}>
- <BooksList />
- </Provider>, document.getElementById('app')
+Routes, document.getElementById('app')
 );
