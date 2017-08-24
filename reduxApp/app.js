0a39e3a4368946f5855612f9f9fc000d781b8c70
mattfro333 <mattfro333@gmail.com>
2017-08-24 13:14:46 -0600

    I appear to be getting a successful post and get

--- reduxApp/app.js
+++ reduxApp/app.js
@@ -6,10 +6,13 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/bookshop');
+Books = require('./models/books.js');
 var app = express();
 // view engine setup
-// app.set('views', path.join(__dirname,'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'jade');
 // uncomment after placing your favicon in/public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,6 +24,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('*', function(req, res){
  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
+app.get('/books', function(req, res){
+ Books.find(function(err, books){
+ if(err){
+ throw err;
+ }
+ res.json(books);
+ })
+});
+//-----POST BOOKS----------
+app.post('/books', function(req, res){
+ var book = req.body;
+ Books.create(book, function(err, books){
+ if(err){
+ throw err;
+ }
+ res.json(books);
+ })
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
  var err = new Error('Not Found');
