eba8753a3a66fe8c17d0b306a70f6d5cb75092ba
mattfro333 <mattfro333@gmail.com>
2017-08-26 22:34:02 -0600

    added some libraries

--- reduxApp/apiServer.js
+++ reduxApp/apiServer.js
@@ -2,6 +2,8 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:
@@ -12,6 +14,34 @@ var mongoose = require('mongoose');
 var promise = mongoose.connect('mongodb://localhost:27017/books', {
 useMongoClient: true,
 });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, '#MongoDB - connection error: '));
+// --->>> SET UP SESSIONS <<<----
+app.use(session({
+ secret: 'mySecretString',
+ saveUninitialized: false,
+ resave:false,
+ store: new MongoStore({mongooseConnection: db, ttl: 2 * 24 * 60 * 60})
+ //ttl: 2 days * 24 hours * 60 minutes * 60 seconds
+}))
+ // SAVE SESSION CART API
+ app.post('/cart', function(req, res){
+ var cart = req.body;
+ req.session.cart = cart;
+ req.session.save(function(err){
+ if(err){
+ throw err;
+ }
+ res.json(req.session.cart);
+ })
+ });
+ // GET SESSION CART API
+ app.get('/cart', function(req, res){
+ if(typeof req.session.cart !==
+'undefined'){
+ res.json(req.session.cart);
+ }
+ });
 var Books = require('./models/books.js');
 //---->>> POST BOOKS <<<-----
 app.post('/books', function(req, res){
